9569c8df83f627eb294914d18d28d8d0cd8278d7
EpocDotFr <contact@epoc.fr>
2026-08-12 17:46:18 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# Reddit feeds cache
+
+We're all tired of Reddit's enshittification. Every month, Reddit is increasingly turning into a data silo, just like
+Twitter, Facebook or Digg before it. One day, not a single byte of data will be able to quit Reddit without any form of
+authentication. For us open data lovers, Atom feeds are still provided — for now, although these have been severely
+rate-limited in June 2026 (one request per minute. Yes, *PER MINUTE*).
+
+So I created this simple script. I want to keep getting updates on things I find interesting without having to visit
+Reddit every day — all while staying within their darn rate limit. I do not want to create an account; I do not want to
+undergo their ads, nor their new design (yes, there's still old.reddit.com, but its days also are numbered).
+
+## Prerequisites
+
+  - Python >= 3.11
+  - A web server
+
+## Installation
+
+Clone this repo somewhere.
+
+Also, you could download `fetch.py` only if you do not want to bother with Git since everything is self-contained in
+that script (look ma, no dependencies!).
+
+## Configuration
+
+Configuration happens through the `config.toml` file, which **must** be located next to `fetch.py`. You will find an
+example configuration file (`config.example.toml`) to start with, everything is explained there.
+
+Your web server (or its virtual host) must point its root directory to the `public` folder which is automatically created
+next to `fetch.py`. Make sure it correctly serve Atom (`*.atom`) files with proper MIME types.
+
+## Usage
+
+This project consists of one Python script, `fetch.py`, which should be invoked at a regular interval (typically using a
+job scheduler like cron). It will download and save each configured feed into the `public` folder, while respecting the
+rate limit.
+
+For example, fetch feeds every two hours:
+
+```
+0 */2 * * * cd /path/to/the/script && ./fetch.py
+```
+
+You can then subscribe to these feeds using your usual feed reader. They are saved using the following path patterns:
+
+  - Subs: `https://your.site/subs/{name}.atom`
+  - Users: `https://your.site/users/{username}.atom`
+  - Domains: `https://your.site/domain/{domain name}.atom`
+
+Plain old static files: that's all we need.
---
+++ fetch.py
@@ -0,0 +1,218 @@
+#!/usr/bin/env python3
+from urllib.request import Request, urlopen
+from typing import Dict, Any, Optional
+from urllib.parse import urlencode
+from urllib.error import HTTPError
+from pathlib import Path
+from time import sleep
+import tomllib
+import logging
+import enum
+
+logging.basicConfig(level=logging.INFO)
+
+
+@enum.unique
+class Sort(enum.StrEnum):
+    Best = 'best'
+    Hot = 'hot'
+    New = 'new'
+    Top = 'top'
+    Rising = 'rising'
+    Controversial = 'controversial'
+
+    def valid_for_sub(self) -> bool:
+        return self in [Sort.Best, Sort.Top, Sort.Hot, Sort.New, Sort.Rising]
+
+    def valid_for_user(self) -> bool:
+        return self in [Sort.Top, Sort.Hot, Sort.New]
+
+    def valid_for_domain(self) -> bool:
+        return self in [Sort.Hot, Sort.New, Sort.Rising, Sort.Controversial, Sort.Top]
+
+
+@enum.unique
+class TopInterval(enum.StrEnum):
+    Hour = 'hour'
+    Day = 'day'
+    Week = 'week'
+    Month = 'month'
+    Year = 'year'
+    All = 'all'
+
+
+@enum.unique
+class UserFilter(enum.StrEnum):
+    Submitted = 'submitted'
+    Comments = 'comments'
+
+
+def load_config() -> Dict[str, Any]:
+    logging.info('Loading configuration...')
+
+    try:
+        with open('config.example.toml', 'rb') as f:
+            return tomllib.load(f)
+    except FileNotFoundError:
+        logging.critical('config.toml not found, aborting.')
+
+        exit(1)
+
+
+def fetch_feeds(config: Dict[str, Any]) -> None:
+    logging.info('Fetching feeds...')
+
+    fetch_subs_feed(config.get('subs', {}))
+    fetch_users_feed(config.get('users', {}))
+    fetch_domains_feed(config.get('domains', {}))
+
+
+def fetch_subs_feed(subs_config: Dict[str, Any]) -> None:
+    if not subs_config:
+        return
+
+    logging.info('Subs...')
+
+    for sub_name, sub_parameters in subs_config.items():
+        try:
+            sort = Sort(sub_parameters.get('sort', Sort.Best))
+        except ValueError:
+            logging.error(f'{sub_name}: invalid "sort" value')
+
+            continue
+
+        try:
+            top_interval = TopInterval(sub_parameters.get('top_interval', TopInterval.Hour))
+        except ValueError:
+            logging.error(f'{sub_name}: invalid "top_interval" value')
+
+            continue
+
+        download_feed(
+            f'r/{sub_name}/{sort}',
+            Path('subs') / f'{sub_name}.atom',
+            {'t': str(top_interval)} if sort == Sort.Top else None
+        )
+
+
+def fetch_users_feed(users_config: Dict[str, Any]) -> None:
+    if not users_config:
+        return
+
+    logging.info('Users...')
+
+    for user_name, user_parameters in users_config.items():
+        try:
+            filter_ = Sort(user_parameters.get('filter', UserFilter.Submitted))
+        except ValueError:
+            logging.error(f'{user_name}: invalid "filter" value')
+
+            continue
+
+        try:
+            sort = Sort(user_parameters.get('sort', Sort.Best))
+        except ValueError:
+            logging.error(f'{user_name}: invalid "sort" value')
+
+            continue
+
+        try:
+            top_interval = TopInterval(user_parameters.get('top_interval', TopInterval.Hour))
+        except ValueError:
+            logging.error(f'{user_name}: invalid "top_interval" value')
+
+            continue
+
+        download_feed(
+            f'user/{user_name}/{filter_}',
+            Path('users') / f'{user_name}.atom',
+            {'t': str(top_interval)} if sort == Sort.Top else None
+        )
+
+
+def fetch_domains_feed(domains_config: Dict[str, Any]) -> None:
+    if not domains_config:
+        return
+
+    logging.info('Domains...')
+
+    for domain_name, domain_parameters in domains_config.items():
+        try:
+            sort = Sort(domain_parameters.get('sort', Sort.Best))
+        except ValueError:
+            logging.error(f'{domain_name}: invalid "sort" value')
+
+            continue
+
+        try:
+            top_interval = TopInterval(domain_parameters.get('top_interval', TopInterval.Hour))
+        except ValueError:
+            logging.error(f'{domain_name}: invalid "top_interval" value')
+
+            continue
+
+        download_feed(
+            f'domains/{domain_name}/{sort}',
+            Path('domains') / f'{domain_name}.atom',
+            {'t': str(top_interval)} if sort == Sort.Top else None
+        )
+
+
+def download_feed(path: str, destination: Path, query: Optional[Dict[str, Any]] = None) -> None:
+    destination = 'public' / destination
+    destination.parent.mkdir(parents=True, exist_ok=True)
+
+    url = f'https://www.reddit.com/{path}.rss'
+
+    if query:
+        url += f'?{urlencode(query)}'
+
+    logging.info(url)
+
+    request_object = Request(
+        url,
+        headers={
+            'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:153.0) Gecko/20100101 Firefox/153.0',
+        },
+        origin_req_host='www.reddit.com',
+        method='GET'
+    )
+
+    try:
+        with urlopen(request_object) as response:
+            with open(destination, 'wb') as f:
+                f.write(response.read())
+    except HTTPError as e:
+        logging.error(e)
+
+        if e.status == 429:
+            logging.critical('Got rate-limited anyway, aborting goddamn.')
+
+            exit(1)
+        if e.status == 404:
+            logging.error('Feed not found.')
+
+            return
+        elif 'application/atom+xml' not in e.headers.get('Content-Type', ''):
+            logging.error('Did not get an Atom file.')
+
+            return
+
+    logging.info('Sleeping for 60 seconds (sigh)...')
+
+    try:
+        sleep(60)  # Poor's man rate-limiter
+    except KeyboardInterrupt:
+        logging.critical('Aborted')
+
+        exit(1)
+
+
+def run() -> None:
+    fetch_feeds(
+        load_config()
+    )
+
+
+if __name__ == '__main__':
+    run()
